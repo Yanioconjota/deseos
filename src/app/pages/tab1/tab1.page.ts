1c528d33f3d62bec8199595f2cec6f216c3dffce
Janio Isacura <janioisacura@gmail.com>
2020-11-23 15:54:25 -0300

    Lógica de redirección al clickear y agregar lista

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Lista } from 'src/app/models/lista.model';
 import { DeseosService } from 'src/app/services/deseos.service';
 
 @Component({
@@ -49,5 +50,10 @@ export class Tab1Page {
     });
 
     await alert.present();
+  }
+
+  listaSeleccionada(lista: Lista){
+    console.log(lista);
+    this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
   }
 }
